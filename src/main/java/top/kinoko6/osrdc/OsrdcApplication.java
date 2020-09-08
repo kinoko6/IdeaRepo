115c3d0bcc9cba47265c9f3e2b83b3da598b985b
tongsy <877399825@qq.com>
2020-09-08 16:56:04 +0800

    第一次提交

--- src/main/java/top/kinoko6/osrdc/OsrdcApplication.java
+++ src/main/java/top/kinoko6/osrdc/OsrdcApplication.java
@@ -8,6 +8,7 @@ public class OsrdcApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(OsrdcApplication.class, args);
+        System.out.println("hello git");
     }
 
 }
