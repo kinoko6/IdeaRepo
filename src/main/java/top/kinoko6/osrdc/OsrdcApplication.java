4060c5fd439d819eea5ccbb3f13521bf06781d24
tongsy <877399825@qq.com>
2020-09-08 16:44:10 +0800

    第一次提交到版本库

---
+++ src/main/java/top/kinoko6/osrdc/OsrdcApplication.java
@@ -0,0 +1,13 @@
+package top.kinoko6.osrdc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OsrdcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OsrdcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/top/kinoko6/osrdc/OsrdcApplicationTests.java
@@ -0,0 +1,13 @@
+package top.kinoko6.osrdc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OsrdcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
